6dff3e1d887214438be380db73f288d1cf28da13
Youssef-gho <ghoneim9000@gmail.com>
2023-05-29 23:12:44 +0300

    last

--- script(contact).js
+++
@@ -1,7 +0,0 @@
-const goHomeLogin = () =>{
-    window.location = "./main.html"
-}
-
-const goServMain = () =>{
-    window.location = "./Services(main).html"
-}
--- script(contactLogin).js
+++
@@ -1,7 +0,0 @@
-const goHomeLogin = () =>{
-    window.location = "./page2.html"
-}
-
-const goServMain = () =>{
-    window.location = "./Services.html"
-}
