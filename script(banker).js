197e322bd87832fd26da6d204937ad2c4ef579aa
Aladdin-O <alaa.ossama10@gmail.com>
2023-05-29 15:27:59 +0200

    fixed replacements

--- script(banker).js
+++ script(banker).js
@@ -5,6 +5,19 @@ cardbtn.addEventListener('click',()=>{
     window.location.href = 'cardApps.html';
 })
 
-const Resolve = (id)=>{
-    window.location.href='main.html'
+function accResolve(accept,freeze,reject){
+    accept.disabled=true;
+    freeze.style.display="none";
+    reject.style.display="none";
 }
+function frzResolve(accept,freeze,reject){
+    freeze.disabled=true;
+    accept.style.display="none";
+    reject.style.display="none";
+}
+function rejResolve(accept,freeze,reject){
+    reject.disabled=true;
+    freeze.style.display="none";
+    accept.style.display="none";
+}
+
